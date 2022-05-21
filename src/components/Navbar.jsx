fb89d40f332423e3905ba4167782f997d6ff3f29
ehsan <ehsansaraee6570@yahoo.com>
2022-05-21 11:22:30 +0430

    create navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,44 @@
+import { useCart } from 'hooks/cart';
+import {
+   MDBBadge,
+   MDBContainer,
+   MDBIcon,
+   MDBNavbar,
+   MDBNavbarBrand,
+   MDBNavbarLink,
+} from 'mdb-react-ui-kit';
+
 const Navbar = () => {
+   const { totalQuantity } = useCart();
+
    return (
-      <>
-         <h1>navbar</h1>
-      </>
+      <MDBNavbar expand="lg" dark bgColor="primary">
+         <MDBContainer>
+            <MDBNavbarBrand
+               style={{
+                  alignItems: 'center',
+                  display: 'flex',
+                  justifyContent: 'space-between',
+               }}
+            >
+               Shopping Cart
+            </MDBNavbarBrand>
+            <MDBNavbarLink>
+               <a className="mx-3">
+                  <MDBIcon fas icon="shopping-cart" size="lg" color="white">
+                     <MDBBadge
+                        color="danger"
+                        pill
+                        notification
+                        style={{ fontSize: '12px' }}
+                     >
+                        {totalQuantity}
+                     </MDBBadge>
+                  </MDBIcon>
+               </a>
+            </MDBNavbarLink>
+         </MDBContainer>
+      </MDBNavbar>
    );
 };
 
